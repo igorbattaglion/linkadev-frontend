71b8d244b1474d2663c278618e0eee7837fe9806
Igor Battaglion Costa <igorbattaglion@gmail.com>
2019-11-26 17:20:46 -0300

    Instalado o materialize

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { TestSpan, NavBar, MainContent, Content, UserArea } from './styles'
+import { Dropdown, NavItem } from 'react-materialize';
+
 function Navbar() {
 
   return (
@@ -9,15 +11,12 @@ function Navbar() {
                 <TestSpan>Side one</TestSpan>
             </Content>
             <UserArea>
-                <TestSpan for="users">Side two</TestSpan>
-                <ul id='users'>
-                    <li>
-                        testando
-                    </li>
-                    <li>
-                        testando 2
-                    </li>
-                </ul>
+                <Dropdown trigger={
+                  <img src={'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'}></img>
+                }>
+                  <NavItem >Contas</NavItem>
+                  <NavItem >Sair</NavItem>
+                </Dropdown>
             </UserArea>
         </MainContent>
         
